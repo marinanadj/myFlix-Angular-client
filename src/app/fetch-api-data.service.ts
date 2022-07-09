d8b2d42825bfd2660dcd714e8d3ba443f12a187a
Marina Nadj <marinanadj7@yahoo.com>
2022-07-09 15:20:12 +0200

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 // Declaring the API URL that provides data for the application
-const apiUrl = 'https://femmovies.herokuapp.com/'
+const apiUrl = 'https://marinanadj-53303.herokuapp.com/'
 
 // Get Authorization token stored in local storage
 const token = localStorage.getItem('token');
